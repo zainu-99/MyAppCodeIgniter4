dd248944b160ddf0cdd4d032bee15b7f58087311
zainu-99 <zainu.developer@gmail.com>
2020-12-01 16:42:54 +0700

    Development

---
+++ app/Helpers/general_helper.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function initialize_elfinder($value=''){
+
+ $CI =& get_instance();
+
+ $CI->load->helper('path');
+
+ $opts = array(
+
+    //'debug' => true,
+
+    'roots' => array(
+
+      array(
+
+        'driver' => 'LocalFileSystem',
+
+        'path'   => './files/',
+
+        'URL'    => site_url('files').'/'
+
+        // more elFinder options here
+
+      )
+
+    )
+
+ );
+
+ return $opts;
+
+}
+
+?>
